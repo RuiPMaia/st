51032c83008dbd079194b2fb29e916b4c50f7350
RuiPMaia <rui.p.maia@tecnico.ulisboa.pt>
2022-12-12 02:13:14 +0000

    fixed config.h

--- config.h
+++ config.h
@@ -5,13 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-<<<<<<< HEAD
 static char *font = "Inconsolata:size=16:antialias=true:hinting=true:autohint=false:hintstyle=1";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-=======
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
->>>>>>> 36d225d71d448bfe307075580f0d8ef81eeb5a87
+// static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+// static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
